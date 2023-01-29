866c32f025f2e32004c047b2280e3ddef5722f37
zhangkaili <zhangkaili@gsafety.com>
2023-01-29 15:28:50 +0800

    子系统接口新增

--- api/v1/voyager1/vo1_system.go
+++ api/v1/voyager1/vo1_system.go
@@ -1,33 +1,40 @@
 package voyager1
 
-import "github.com/zhangrt/voyager1_platform/model/system"
+import (
+	"github.com/zhangrt/voyager1_platform/global"
+	"github.com/zhangrt/voyager1_platform/model/system"
+)
 
 type SystemApi struct{}
 
-func (s *SystemApi) GetSystemModel(id string) (system system.Vo1System) {
-	return
+// @Tags Vo1System
+// @Summary 根据子系统Id获取子系统信息
+// @accept application/json
+// @Produce application/json
+// @Param data body systemReq.SetId true "子系统Id"
+// @Success 200 {object} response.Response{msg=string} "子系统信息"
+// @Router /system/getSystemModel [post]
+func (s *SystemApi) GetSystemModel(id string) (_ system.Vo1System, err error) {
+	var system system.Vo1System
+	err = global.GS_DB.Where("global.GS_BASE_MODEL_ID_STRING.ID = ? and deleted <> 1", id).Error
+	if err != nil {
+		return *&system, err
+	}
+	return system, err
 }
 
-func (s *SystemApi) CreateSystemModel(systemReq system.Vo1System) (systemRes system.Vo1System) {
-	return
-}
-
-func (s *SystemApi) UpdateSystemModel(id string, systemReq system.Vo1System) (systemRes system.Vo1System) {
-	return
-}
-
-func (s *SystemApi) DeleteSystemModel(id string) (res bool) {
-	return
-}
-
-func (s *SystemApi) GetSystemsRolesModel() (system *system.Vo1System) {
-	return
-}
-
-func (s *SystemApi) GetSystemsRolesModelById(id string) (system *system.Vo1System) {
-	return
-}
-
-func (s *SystemApi) UpdateSystemRolesModel(id string, roleSystemReq *system.Vo1RoleSystem) (roleSystemRes *system.Vo1RoleSystem) {
-	return
+// @Tags Vo1System
+// @Summary 新增或更新子系统
+// @accept application/json
+// @Produce application/json
+// @Param data body systemReq.CreateSystemModel true "子系统模型"
+// @Success 200 {object} response.Response{msg=string} "子系统模型"
+// @Router /system/createSystemModel [post]
+func (s *SystemApi) CreateSystemModel(systemReq system.Vo1System) (_ system.Vo1System, err error) {
+	var system system.Vo1System
+	err = global.GS_DB.Save(system).Error
+	if err != nil {
+		return *&system, err
+	}
+	return system, err
 }
